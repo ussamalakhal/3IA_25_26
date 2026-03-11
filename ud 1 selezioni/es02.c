457fe8522a5b66d2018a563f4727c74417919398
ussamalakhal <oussama.lakhal@galileiostiglia.edu.it>
2026-03-11 11:45:31 +0000

    verifica_funzione

---
+++ ud 1 selezioni/es02.c
@@ -0,0 +1,37 @@
+/* Contiene il main program dell'esercizio che voglio svolgere */
+#include <stdio.h>
+#include "mialib.c"
+
+int main(){
+    int valore;
+    int junk;
+    for(valore=1; valore <= 7; valore++){
+        if(isPrimo(valore))
+            printf("%d -> Primo\n", valore);
+    }
+    printf("\n\n");
+    for(valore=7; valore <= 15; valore++){
+        if(isPrimo(valore))
+            printf("%d -> Primo\n", valore);
+    }
+    printf("\n\n");
+    for(valore=15; valore <= 25; valore++){
+        if(isPrimo(valore))
+            printf("%d -> Primo\n", valore);
+    }
+    printf("\n\n");
+    for(valore = 1; valore<=30; valore++){
+        if(isPerfect(valore))
+            printf("%d -> Perfetto\n", valore);
+    }
+
+    // verifico funzione terna pitagorica
+    if(isTernaPitagorica(3,4,5))
+        printf("I valori 3,4,5 sono una terna pitagorica.\n");
+    else
+        printf("I valori 3,4,5 non sono una terna pitagorica.\n");
+    // verifico la funzione inserimento controllato di un valore intero
+    tmp= inputValoreRange(5, 15);
+    printf("valore inserito nel range : %d\n", tmp);
+    return(0); 
+}
---
+++ ud 1 selezioni/mialib.h
@@ -0,0 +1,40 @@
+/*  Contiene solo i prototipi delle funzione che vado a sviluppare
+    per la mia libreria personale */
+
+/**
+ * Check whether a number is prime or not.
+ * @param int Number to test.
+ * @return boolean true/false.
+ */
+bool isPrimo(int _n);
+/**
+ * Verifica se un numero è perfetto oppure no
+ * @param int Numero da verificare
+ * @return boolean true/false
+ */
+bool isPerfect(int _n);
+/**
+ * Verifica terna pitagorica, se n1^2+n2^2 = n3^2
+ * @param int _n1 Primo valore
+ * @param int _n2 Secondo valore
+ * @param int _n3 Terzo valore
+ * @return boolean true/false
+ */
+bool isTernaPitagorica(int _n1, int _n2, int _n3);
+
+
+/**
+ * Inserimento controllato di un valore intero tra min e max estremi compresi
+ * @param int Valore minimo accettato
+ * @param int Valore massimo accettato
+ * @return Valore inserito dall'utente valido e nel range min-max
+ */
+int inputValoreRange(int _min, int _max);
+
+/**
+ * Genera un valore random compreso tra due valori interi min e max, estremi compresi
+ * @param int Valore minimo accettato
+ * @param int Valore massimo accettato
+ * @return Valore random generato compreso nel range min-max
+ */
+int randomValoreRange(int _min, int _max);
